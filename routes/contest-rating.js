4cab3905188c924571dd8797e4b18bb5306100c7
akshay722 <akshayvarmamit@gmail.com>
2022-02-03 16:23:05 +0530

    Final Update

--- routes/contest-rating.js
+++ routes/contest-rating.js
@@ -5,61 +5,66 @@ const fetch = require("node-fetch");
 var { requests } = require("../services/requests");
 
 router.get("/", function (req, res, next) {
-    res.send("contest-rating get ... ");
+  res.send("contest-rating get ... ");
 });
 
 const fetchData = async (req, res) => {
-    let {
-        operationName = "getContestRankingData", // with default
-        username = "pgmreddy", // with default
-    } = req.body;
+  let {
+    operationName = "getContestRankingData", // with default
+    username = "akshayvarmamit", // with default
+  } = req.body;
 
-    // let {
-    //     operationName = "getUserProfile", // with default
-    //     username = "pgmreddy", // with default
-    // } = req.body
+  // let {
+  //     operationName = "getUserProfile", // with default
+  //     username = "", // with default
+  // } = req.body
 
-    let { url, method, headers, body } = JSON.parse(JSON.stringify(requests.getContestRankingData));
+  let { url, method, headers, body } = JSON.parse(
+    JSON.stringify(requests.getContestRankingData)
+  );
 
-    headers.referer = headers.referer.replace("{USER_NAME}", username);
-    body.variables.username = body.variables.username.replace("{USER_NAME}", username);
+  headers.referer = headers.referer.replace("{USER_NAME}", username);
+  body.variables.username = body.variables.username.replace(
+    "{USER_NAME}",
+    username
+  );
 
-    const response = await fetch(
-        url, //
-        {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            // mode: 'cors', // no-cors, *cors, same-origin
-            // mode: 'no-cors', // no-cors, *cors, same-origin
-            // mode: 'same-origin', // no-cors, *cors, same-origin
-            // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            // credentials: 'same-origin', // include, *same-origin, omit
-            headers: headers,
-            // {
-            //   'Content-Type': 'application/json'
-            //   // 'Content-Type': 'application/x-www-form-urlencoded',
-            // },
-            // redirect: 'follow', // manual, *follow, error
-            // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            // body: JSON.stringify(data) // body data type must match "Content-Type" header
-            body: JSON.stringify(body), // body data type must match "Content-Type" header
-        }
-    );
+  const response = await fetch(
+    url, //
+    {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      // mode: 'cors', // no-cors, *cors, same-origin
+      // mode: 'no-cors', // no-cors, *cors, same-origin
+      // mode: 'same-origin', // no-cors, *cors, same-origin
+      // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      // credentials: 'same-origin', // include, *same-origin, omit
+      headers: headers,
+      // {
+      //   'Content-Type': 'application/json'
+      //   // 'Content-Type': 'application/x-www-form-urlencoded',
+      // },
+      // redirect: 'follow', // manual, *follow, error
+      // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      // body: JSON.stringify(data) // body data type must match "Content-Type" header
+      body: JSON.stringify(body), // body data type must match "Content-Type" header
+    }
+  );
 
-    let resp = await response.json();
-    return resp;
+  let resp = await response.json();
+  return resp;
 };
 
 router.post("/", async function (req, res, next) {
-    // let fromUI = {
-    //     // url: req.url,
-    //     // method: req.method,
-    //     // headers: req.headers,
-    //     body: req.body,
-    // };
+  // let fromUI = {
+  //     // url: req.url,
+  //     // method: req.method,
+  //     // headers: req.headers,
+  //     body: req.body,
+  // };
 
-    let a = await fetchData(req, res);
+  let a = await fetchData(req, res);
 
-    res.send(a);
+  res.send(a);
 });
 
 module.exports = router;
--- routes/user-profile.js
+++ routes/user-profile.js
@@ -6,65 +6,70 @@ var { requests } = require("../services/requests");
 // console.log(url, method, headers, body);
 
 router.get("/", function (req, res, next) {
-    res.send("user-profile get ... ");
+  res.send("user-profile get ... ");
 });
 
 const fetchData = async (req, res) => {
-    let {
-        operationName = "getUserProfile", // with default
-        username = "pgmreddy", // with default
-    } = req.body;
+  let {
+    operationName = "getUserProfile", // with default
+    username = "akshayvarmamit", // with default
+  } = req.body;
 
-    let { url, method, headers, body } = JSON.parse(JSON.stringify(requests.getUserProfile));
+  let { url, method, headers, body } = JSON.parse(
+    JSON.stringify(requests.getUserProfile)
+  );
 
-    headers.referer = headers.referer.replace("{USER_NAME}", username);
-    body.variables.username = body.variables.username.replace("{USER_NAME}", username);
+  headers.referer = headers.referer.replace("{USER_NAME}", username);
+  body.variables.username = body.variables.username.replace(
+    "{USER_NAME}",
+    username
+  );
 
-    // console.log(username);
-    // console.log(headers);
-    // console.log(body);
+  // console.log(username);
+  // console.log(headers);
+  // console.log(body);
 
-    const response = await fetch(
-        url, //
-        {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            // mode: 'cors', // no-cors, *cors, same-origin
-            // mode: 'no-cors', // no-cors, *cors, same-origin
-            // mode: 'same-origin', // no-cors, *cors, same-origin
-            // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            // credentials: 'same-origin', // include, *same-origin, omit
-            headers: headers,
-            // {
-            //   'Content-Type': 'application/json'
-            //   // 'Content-Type': 'application/x-www-form-urlencoded',
-            // },
-            // redirect: 'follow', // manual, *follow, error
-            // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            // body: JSON.stringify(data) // body data type must match "Content-Type" header
-            body: JSON.stringify(body), // body data type must match "Content-Type" header
-        }
-    );
+  const response = await fetch(
+    url, //
+    {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      // mode: 'cors', // no-cors, *cors, same-origin
+      // mode: 'no-cors', // no-cors, *cors, same-origin
+      // mode: 'same-origin', // no-cors, *cors, same-origin
+      // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      // credentials: 'same-origin', // include, *same-origin, omit
+      headers: headers,
+      // {
+      //   'Content-Type': 'application/json'
+      //   // 'Content-Type': 'application/x-www-form-urlencoded',
+      // },
+      // redirect: 'follow', // manual, *follow, error
+      // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      // body: JSON.stringify(data) // body data type must match "Content-Type" header
+      body: JSON.stringify(body), // body data type must match "Content-Type" header
+    }
+  );
 
-    let resp = await response.json();
-    // console.log("101", response);
-    // console.log(await response.text()); // parses JSON response into native JavaScript objects
-    // console.log(resp); // parses JSON response into native JavaScript objects
-    // console.log("102");
-    return resp;
+  let resp = await response.json();
+  // console.log("101", response);
+  // console.log(await response.text()); // parses JSON response into native JavaScript objects
+  // console.log(resp); // parses JSON response into native JavaScript objects
+  // console.log("102");
+  return resp;
 };
 
 router.post("/", async function (req, res, next) {
-    // let fromUI = {
-    //     // url: req.url,
-    //     // method: req.method,
-    //     // headers: req.headers,
-    //     body: req.body,
-    // };
+  // let fromUI = {
+  //     // url: req.url,
+  //     // method: req.method,
+  //     // headers: req.headers,
+  //     body: req.body,
+  // };
 
-    let a = await fetchData(req, res);
-    // console.log(a);
+  let a = await fetchData(req, res);
+  // console.log(a);
 
-    res.send(a);
+  res.send(a);
 });
 
 module.exports = router;
